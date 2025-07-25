a820802f67853d979a29ff2ffecb4034eb9627cf
jk2k <4025839+jk2k@users.noreply.github.com>
2025-07-25 17:46:04 +0800

    initial commit

---
+++ components/Test.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { FwbButton } from 'flowbite-vue'
+</script>
+
+<template>
+    <div class="zhichunQRTool">
+        <fwb-button color="default">Default</fwb-button>
+    </div>
+</template>
+
+<style>
+</style>
---
+++ docs/flowbite.md
@@ -0,0 +1,6 @@
+# button
+<script setup>
+import Test from '../components/Test.vue'
+</script>
+
+<Test />

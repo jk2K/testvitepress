db296c8f1fefb3d12f10d5551604a197f45c8ec3
jk2k <4025839+jk2k@users.noreply.github.com>
2025-07-26 12:39:58 +0800

    fix: grid not work

--- components/Test.vue
+++ components/Test.vue
@@ -1,12 +1,18 @@
 <script setup>
-import { FwbButton } from 'flowbite-vue'
+import { FwbButton } from "flowbite-vue";
 </script>
 
 <template>
     <div class="">
         <fwb-button color="default">Default</fwb-button>
+
+        <div class="grid grid-cols-2 gap-4">
+            <div>01</div>
+            <div>02</div>
+            <div>03</div>
+            <div>04</div>
+        </div>
     </div>
 </template>
 
-<style>
-</style>
+<style></style>
